ca338f6dbe2f155be7995469feb4f31eaa2297e4
nick-fournier <45876721+nick-fournier@users.noreply.github.com>
2026-04-23 13:52:07 -0700

    fix sphinx doc bug

--- docs/conf.py
+++ docs/conf.py
@@ -16,12 +16,12 @@
 import sphinx_rtd_theme
 
 # -- Get Package Version --------------------------------------------------
-with open("../setup.py") as file:
-    lines = file.readlines()
-    for line in lines:
-        if "version" in line:
-            VERSION = line.replace("version='", "").replace("',", "").replace(" ", "")
-            print("package version: " + VERSION)
+try:
+    from importlib.metadata import version as get_version
+    VERSION = get_version("populationsim")
+except Exception:
+    VERSION = "unknown"
+print("package version: " + VERSION)
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the

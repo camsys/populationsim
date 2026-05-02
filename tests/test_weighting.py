aaed68c6ea440ef54b5cfd78c8973005683006c5
JoeJimFlood <joejimflood@gmail.com>
2026-05-01 17:06:31 -0700

    Reapply "Removed lines causing linting issues"
    This reverts commit 0fb8842f284202a1af12096833432d95be209fa8.

--- tests/test_weighting.py
+++ tests/test_weighting.py
@@ -19,7 +19,6 @@ def test_weighting():
     configs_dir = example_dir / "configs"
     data_dir = example_dir / "data"
     output_dir = Path(__file__).parent / "output"
-    expect_dir = Path(__file__).parent / "expected"
 
     inject.reinject_decorated_tables()
 
